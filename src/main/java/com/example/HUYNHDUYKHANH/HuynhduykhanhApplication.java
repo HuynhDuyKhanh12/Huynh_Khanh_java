36cf5abb3bc4d66f5215cb425f83c7662bf1630b
HuynhKhanh <huynhkhanh23122003@gmail.com>
2025-02-24 10:29:46 +0700

    init project

---
+++ src/main/java/com/example/HUYNHDUYKHANH/HuynhduykhanhApplication.java
@@ -0,0 +1,13 @@
+package com.example.HUYNHDUYKHANH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuynhduykhanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuynhduykhanhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HUYNHDUYKHANH/HuynhduykhanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HUYNHDUYKHANH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuynhduykhanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
